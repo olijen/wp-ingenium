43bcf8acd1218cffd2dfaa26204ff0e2f295ae65
Oleg Grigoryev <olegg@taxer.ua>
2017-03-03 19:46:56 +0200

    #LOC-3 автоматизировал деплой клиент-приложения, добавил пару компонентов ангуляр для проверки.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+const todos = [
+    'some TODO',
+    'some TODO',
+    'some TODO',
+    'some TODO',
+];
+const actions = [
+
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'TODO list';
+  todos = todos;
+  actions = actions;
 }
